9f09dd2c838fd97e9c5840e74a1158b28114786b
AdrianHsu <adrianhsu1995@gmail.com>
2017-03-31 12:50:29 +0800

    done

--- hw2/src_P1/P1Client.java
+++ hw2/src_P1/P1Client.java
@@ -6,7 +6,8 @@ public class P1Client {
    private Socket clientSocket = null;
    private BufferedReader inFromServer = null;
    private int packageCount = 0;
-
+   private int totalCount = 5;
+   
    public void connectToServer() throws IOException {
       String serverAddress = "127.0.0.1";
       //serverAddress = "140.112.18.178";
@@ -20,15 +21,18 @@ public class P1Client {
    }
    public void start() throws Exception {
       // TODO: handle messages recv from and send to server
+      
       while(true) {
          String recvFrom = inFromServer.readLine();
          System.out.println(recvFrom);
+         if(packageCount == totalCount - 1) break;
          DataOutputStream outToServer =
   				  	new DataOutputStream(clientSocket.getOutputStream());
          BufferedReader buf = new BufferedReader(
                                       new InputStreamReader(System.in));
          String text = buf.readLine();
          outToServer.writeBytes(text + '\n');
+         packageCount++;
       }
    }
    /**
