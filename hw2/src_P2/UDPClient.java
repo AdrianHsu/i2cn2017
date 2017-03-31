fccbec14344158e6376f993d0a8cd376b79f04c7
AdrianHsu <adrianhsu1995@gmail.com>
2017-03-31 21:08:17 +0800

    aws

--- hw2/src_P2/UDPClient.java
+++ hw2/src_P2/UDPClient.java
@@ -6,18 +6,25 @@ class UDPClient {
     public static void main(String args[]) throws Exception {
         DatagramSocket clientSocket = new DatagramSocket();
 
-        // Send the sentence to Server 1000 times continuously
-        InetAddress serverIP = InetAddress.getByName("127.0.0.1");
+        // 1. LOCAL: Send the sentence to Server 10000 times continuously
+        // InetAddress serverIP = InetAddress.getByName("127.0.0.1");
+
+        // 2. mslab workstation (CSIE Department, prof. Shou-De Lin)
+        // InetAddress serverIP = InetAddress.getByName("140.112.31.184");
+
+        // 3. Amazon AWS EC2 Services (Zone: us-west-2a, 52.41.77.129)
+        InetAddress serverIP = InetAddress.getByName("52.41.77.129");
 
         for(int i = 1; i <= 10000; ++i) {
             String sentence = "Hello from Client, Index of this package: " + i;
+            System.out.println("Client Side SENT: Index of this package: " + i);
             byte[] bytes = sentence.getBytes();
             DatagramPacket sendPkt =
                 new DatagramPacket(bytes, bytes.length, serverIP, 9091);
             clientSocket.send(sendPkt);
 
             /* Suspend for 1us */
-            // Thread.sleep(1); /*Prevent client side buffer overflow. */
+            Thread.sleep(1); /*Prevent client side buffer overflow. */
         }
 
         clientSocket.close();
